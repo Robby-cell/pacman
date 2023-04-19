a3e345b499ae29a3fa77652be9324ec6eb474086
Robby-cell <61801830+Robby-cell@users.noreply.github.com>
2023-04-19 12:33:46 +0100

    changes to match other changes from other files

--- src/game.rs
+++ src/game.rs
@@ -1,16 +1,20 @@
+use std::vec;
+
 use graphics::glyph_cache::rusttype::GlyphCache;
 use graphics::Transformed;
 use opengl_graphics::Filter;
 use opengl_graphics::GlGraphics;
 use opengl_graphics::OpenGL;
 use opengl_graphics::TextureSettings;
-use piston::input::*;
+use piston::input::{Button, RenderArgs};
 
 use crate::enemy::*;
 use crate::map::Wall;
-use crate::map::WallType;
 use crate::player;
 use crate::points;
+use crate::utilities::Direction;
+use crate::utilities::ENEMY_SIZE;
+use crate::utilities::PLAYER_SIZE;
 
 use player::*;
 use points::*;
@@ -21,6 +25,7 @@ pub struct Game {
     pub score: i32,
     pub collects: Box<Vec<Option<Point>>>,
     pub ghosts: Box<[Option<Ghost>; 4]>,
+    wall_gl: GlGraphics,
     walls: Box<[Wall]>,
     //glyph_cache: GlyphCache,
 }
@@ -31,26 +36,58 @@ impl Game {
             gl,
             player: Player::new(x, y, health),
             score: 0,
-            collects: Box::new(vec![Some(Point::new(50., 70., 1))]),
+            collects: Box::new(vec![Some(Point::new(500., 700., 1, [1., 1., 1., 1.]))]),
             ghosts: Box::new([
-                Some(Ghost::new(67., 56., 2., 1., Color::Red)),
+                Some(Ghost::new(300., 300., 2., Color::Red)),
                 None,
                 None,
                 None,
             ]),
-            walls: Box::new([Wall::new(
-                OpenGL::V4_2,
-                WallType::Horizontal,
-                vec![(40., 40.), (60., 60.)],
-                vec![60., 20.],
-                vec![20., 40.],
-                [0., 0., 1., 1.0],
-            )]), //glyph_cache: GlyphCache::new(),
+            wall_gl: GlGraphics::new(OpenGL::V4_2),
+            walls: Box::new([
+                Wall::new((40., 40.), 360., 60., [0., 0., 1., 1.]),
+                Wall::new((600., 200.), 60., 300., [0., 0., 1., 1.]),
+            ]), //glyph_cache: GlyphCache::new(),
         }
     }
 
     pub fn update(&mut self) {
         self.player.update();
+        for ghost in self.ghosts.iter_mut() {
+            if let Some(g) = ghost {
+                for wall in self.walls.iter_mut() {
+                    if g.x - ENEMY_SIZE >= wall.x0
+                        && g.x <= wall.x1
+                        && g.y - ENEMY_SIZE >= wall.y0
+                        && g.y <= wall.y1
+                    {
+                        g.moving = false;
+                        g.rethink();
+                        if g.x <= wall.x0 && g.direction != Direction::Right
+                            || g.x >= wall.x1 && g.direction != Direction::Left
+                            || g.y >= wall.y1 && g.direction != Direction::Up
+                            || g.y <= wall.y0 && g.direction != Direction::Down
+                        {
+                            g.moving = true;
+                        }
+                    }
+                }
+                g.update();
+            }
+        }
+
+        for point in self.collects.iter_mut() {
+            if let Some(p) = point {
+                if p.x > self.player.x - p.size
+                    && p.x < self.player.x + PLAYER_SIZE
+                    && p.y > self.player.y - p.size
+                    && p.y < self.player.y + PLAYER_SIZE
+                {
+                    self.score += p.worth;
+                    *point = None;
+                }
+            }
+        }
     }
 
     pub fn render(&mut self, args: &RenderArgs) {
@@ -59,12 +96,12 @@ impl Game {
 
             let texture_settings: TextureSettings = TextureSettings::new().filter(Filter::Nearest);
             let ref mut glyphs =
-                GlyphCache::new("assets/RoadPixel.ttf", (), texture_settings).unwrap();
+                GlyphCache::new("./assets/fonts/RoadPixel.ttf", (), texture_settings).unwrap();
 
             graphics::text(
                 [1., 0., 0., 1.],
                 22,
-                &format!("Score: {}", self.score),
+                format!("Score: {}", self.score).as_str(),
                 glyphs,
                 c.transform.trans(50., 250.),
                 gl,
@@ -78,10 +115,16 @@ impl Game {
             }
 
             self.player.render(gl, args);
+
+            for ghost in self.ghosts.iter_mut() {
+                if let Some(g) = ghost {
+                    g.render(args);
+                }
+            }
         });
 
         for wall in self.walls.iter_mut() {
-            wall.render(args);
+            wall.render(args, &mut self.wall_gl);
         }
     }
 
